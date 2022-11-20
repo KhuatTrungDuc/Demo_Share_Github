5430e1b9a32634e4ed3eafaa645ae07b221dfe7d
Trung Đức <trungduc@MacBook-Pro-cua-Trung.local>
2022-11-20 10:14:14 +0700

    Initial commit

---
+++ app/src/main/java/ducktph19569/fpoly/demo_share_github/MainActivity.java
@@ -0,0 +1,14 @@
+package ducktph19569.fpoly.demo_share_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
