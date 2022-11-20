bb579ebfa628e83ad1318f0fff8a58bb0fe8182b
KhuatTrungDuc <ducktph19569@fpt.edu.vn>
2022-11-20 10:40:36 +0700

    update code LoginActivity

---
+++ app/src/main/java/ducktph19569/fpoly/demo_share_github/LoginActivity.java
@@ -0,0 +1,17 @@
+package ducktph19569.fpoly.demo_share_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+    String a="avc";
+}
